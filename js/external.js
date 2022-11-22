967424f1cdd8ee5c43341e77a1a24545cbb9f5e9
Jennie Smith <Jsmith9spiral@gmail.com>
2022-11-22 10:41:20 -0600

    External html & js

---
+++ js/external.js
@@ -0,0 +1,14 @@
+/*alert('Welcome to my Website!');
+var input = prompt("What is your favorite color?");
+alert(input + " is my favorite color too!");*/
+alert("Enter how many days you will rent the given movie:")
+let mermaid = prompt("The Little Mermaid");
+let brother = prompt("Brother Bear");
+let hercules = prompt("Hercules");
+let mer = mermaid * 3;
+let bro = brother * 3;
+let herc = hercules * 3;
+alert("The Little Mermaid will be  $" + mer);
+alert("Brother Bear will be  $" + bro);
+alert("Hercules will be  $" + herc);
+
