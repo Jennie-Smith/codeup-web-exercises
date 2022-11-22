187f7fa4b90a2cfb6b5088b7b6addc3c703e5701
Jennie Smith <Jsmith9spiral@gmail.com>
2022-11-22 14:57:58 -0600

    function excersices

--- js/external.js
+++ js/external.js
@@ -1,14 +1,17 @@
+"use strict"
 /*alert('Welcome to my Website!');
 var input = prompt("What is your favorite color?");
-alert(input + " is my favorite color too!");*/
-alert("Enter how many days you will rent the given movie:")
+alert(input + " is my favorite color too!");
+alert("Enter how many days you will rent the given movie:")*/
+
 let mermaid = prompt("The Little Mermaid");
 let brother = prompt("Brother Bear");
 let hercules = prompt("Hercules");
 let mer = mermaid * 3;
 let bro = brother * 3;
 let herc = hercules * 3;
+let price = mer + bro + herc;
 alert("The Little Mermaid will be  $" + mer);
 alert("Brother Bear will be  $" + bro);
 alert("Hercules will be  $" + herc);
-
+alert("Total is  $" + price);
