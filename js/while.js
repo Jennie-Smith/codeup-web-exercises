1b8445cae7c3b1699d22f4df45faa2aec3a86dc4
Jennie Smith <Jsmith9spiral@gmail.com>
2022-12-01 11:50:04 -0600

    done

--- js/while.js
+++ js/while.js
@@ -1,9 +1,25 @@
 "use strict"
 
-let i = 1;
+// let i = 1;
+//
+// while (i <= 7000) {
+//     console.log(i);
+//     i += i;
+// }
 
-while (i < 10) {
-    console.log(i);
-    i+2;
-}
 
+var allCones = Math.floor(Math.random() * 50) + 50;
+// This expression will generate a random number between 1 and 5
+
+
+let i = 50;
+
+do {
+    let cones = Math.floor(Math.random() * 5) + 1;
+    console.log(cones + " cones sold. We have " + (i  - cones)+ " left.");
+    i -= cones;
+    i += 1;
+    i--;
+} while (i >= 1);
+
+console.log("No more cones. I can't sell you what you asked.")
