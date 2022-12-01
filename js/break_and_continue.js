22d326e48e36011d96d925cf76ba48156d48c21e
Jennie Smith <Jsmith9spiral@gmail.com>
2022-12-01 11:23:53 -0600

    update

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,1 +1,21 @@
 "use strict"
+
+function skip(){
+    let y = prompt("Please enter an Odd number:")
+    y
+    for (let i = 1; i < 50; i++){
+        if (y % 2 === 0){
+            prompt(y + " is Even. Please enter an Odd number.");
+            break;
+        } else if (y == i){
+            console.log(y + " is your number.")
+            continue;
+        } else {
+            console.log(i + " is an odd number");
+        }
+
+    }
+
+}
+
+skip()
---
+++ js/while.js
@@ -0,0 +1,9 @@
+"use strict"
+
+let i = 1;
+
+while (i < 10) {
+    console.log(i);
+    i+2;
+}
+
