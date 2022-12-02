418f6cce64f0eded55352e11536c7bf4a5c8d316
Jennie Smith <Jsmith9spiral@gmail.com>
2022-12-02 13:51:38 -0600

    array update and objects

--- js/iterating.js
+++ js/iterating.js
@@ -18,25 +18,25 @@
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
-    // console.log(names[0]);
-    // console.log(names[1]);
-    // console.log(names[2]);
-    // console.log(names[3]);
+    console.log(names[0]);
+    console.log(names[1]);
+    console.log(names[2]);
+    console.log(names[3]);
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
-    // for (let i = 0; i < names.length; i++){
-    //     console.log(names[i]);
-    // }
+    for (let i = 0; i < names.length; i++){
+        console.log(names[i]);
+    }
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
-    // names.forEach(function (name) {
-    //     console.log(name);
-    // });
+    names.forEach(function (name) {
+        console.log(name);
+    });
     /**
      * TODO:
      * Create the following three functions, each will accept an array and
@@ -51,17 +51,21 @@
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
     function first(arr){
-        console.log(arr[0]);
+        return arr[0];
     }
     first(names);
 
     function second(arr){
-        console.log(arr[1]);
+         return arr[1];
     }
     second(names);
 
     function last(arr){
-        console.log(arr[arr.length - 1]);
+        return arr[arr.length - 1];
     }
     last(names);
+    const myArray = [1, 2, 3, 4, 5];
+    console.log(first(myArray));
+    console.log(second(myArray));
+    console.log(last(myArray));
 })();
