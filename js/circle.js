df7d94c57cdc147479bc9973e2e0b5a97d4b648e
Jennie Smith <Jsmith9spiral@gmail.com>
2022-12-02 16:32:42 -0600

    circle

---
+++ js/circle.js
@@ -0,0 +1,47 @@
+(function() {
+    "use strict";
+
+    // create a circle object
+    let circle = {
+        radius: 3,
+
+        getArea: function (rad) {
+            // TODO: complete this method
+            // hint: area = pi * radius^2
+            //console.log((Math.pow(rad, 2) * Math.PI));
+            return Math.pow(rad, 2) * Math.PI;
+            // TODO: return the proper value
+        },
+
+        logInfo: function (doRounding) {
+            // TODO: complete this method.
+
+            // If doRounding is true, round the result to the nearest integer.
+            // Otherwise, output the complete value
+            if (doRounding === true){
+                let x = (Math.round(circle.getArea(circle.radius)));
+                console.log("Area of a circle with radius: " + this.radius + ", is: ");
+                console.log(x);
+            } else {
+                console.log(circle.getArea(circle.radius))
+            }
+
+        }
+    };
+
+    // log info about the circle
+    console.log("Raw circle information");
+    circle.logInfo(false);
+    //console.log(circle.getArea(circle.radius));
+    console.log("Circle information rounded to the nearest whole number");
+    circle.logInfo(true);
+
+    console.log("=======================================================");
+    // TODO: Change the radius of the circle to 5.
+
+    // log info about the circle
+    console.log("Raw circle information");
+    circle.logInfo(false);
+    console.log("Circle information rounded to the nearest whole number");
+    circle.logInfo(true);
+})();
