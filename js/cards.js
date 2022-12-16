8f4538d5f5782596afa4adc5d96242359ce9b524
Jennie Smith <Jsmith9spiral@gmail.com>
2022-12-16 14:21:03 -0600

    update

--- js/cards.js
+++ js/cards.js
@@ -2,7 +2,6 @@
 
 let suits = ["clubs", "spades", "diamonds", "hearts"];
 let ranks = ["Ace", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
-
 let deck = new Array();
 
 function getDeck(){
@@ -14,9 +13,8 @@ function getDeck(){
     }
     return deck;
 };
-console.log(getDeck());
 
-function shuffle(deck){
+function shuffle(){
     for(let i = 0; i < 1000; i++){
         let location1 = Math.floor((Math.random() * deck.length));
         let location2 = Math.floor((Math.random() * deck.length));
@@ -27,4 +25,14 @@ function shuffle(deck){
     }
 };
 
-console.log(getDeck());
+// console.log(getDeck());
+let deck1 = getDeck();
+shuffle(deck1);
+console.log(deck1);
+
+function dealCard(deck) {
+    return deck.pop();
+}
+let card = dealCard(deck1);
+console.log(card);
+console.log(deck1);
