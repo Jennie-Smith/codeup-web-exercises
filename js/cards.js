ba8a9cde9bae8d82672a66564dd2ecb28ada79d7
Jennie Smith <Jsmith9spiral@gmail.com>
2022-12-19 21:43:25 -0600

    update

--- js/cards.js
+++ js/cards.js
@@ -1,5 +1,27 @@
 "use strict"
 
+console.log("Let's play Blackjack!!1");
+
+let myscore = 0;
+let computerscore = 0;
+
+console.log(`Your Score: ${myscore} `)
+console.log(`Computer Score: ${computerscore} `)
+
+function mainMenu(){
+    console.log("MAIN MENU");
+    console.log("1. Play Blackjack");
+    console.log("2. Exit Game");
+    let playerInput = prompt("Please enter 1 or 2:");
+    if (playerInput === 1){
+        console.log("Yay! Time for BlackJack!");
+    } else if (playerInput === 2){
+        console.log("Goodbye!");
+    }
+}
+
+console.log(mainMenu());
+
 let suits = ["clubs", "spades", "diamonds", "hearts"];
 let ranks = ["Ace", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
 let deck = new Array();
@@ -19,20 +41,17 @@ function shuffle(){
         let location1 = Math.floor((Math.random() * deck.length));
         let location2 = Math.floor((Math.random() * deck.length));
         let randomCard = deck[location1];
-
         deck[location1] = deck[location2];
         deck[location2] = randomCard;
     }
 };
 
-// console.log(getDeck());
-let deck1 = getDeck();
-shuffle(deck1);
-console.log(deck1);
-
 function dealCard(deck) {
     return deck.pop();
 }
+
+console.log(getDeck());
+let deck1 = getDeck();
+shuffle(deck1);
 let card = dealCard(deck1);
 console.log(card);
-console.log(deck1);
